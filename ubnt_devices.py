a41e6f312f25d4b11462b7997cb7ddcf98c0e08c
nathanshimp <johnnshimp@gmail.com>
2015-04-18 22:23:41 -0400

    Made __getattr__ and __setattr__ safe and smart with error handling

--- ubnt_devices.py
+++ ubnt_devices.py
@@ -5,76 +5,92 @@ from paramiko.ssh_exception import SSHException
 from paramiko.ssh_exception import AuthenticationException
 from paramiko.ssh_exception import BadHostKeyException
 
-#TODO Improve __setattr__ for human error
-
 
 class Device(object):
     def __init__(self, host, username, password):
-        self.host = host
-        self.username = username
-        self.password = password
-        self.session = SSHClient()
+        self.__dict__["host"] = host
+        self.__dict__["username"] = username
+        self.__dict__["password"] = password
+        self.__dict__["session"] = SSHClient()
+        self.__dict__["transport"] = self.session.get_transport()
         self.session.set_missing_host_key_policy(AutoAddPolicy())
         self.connect()
-        self.transport = self.session.get_transport()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.disconnect()
+
+    def __repr__(self):
+        return '{0}{1}'.format(self.__class__.__name__, self.host)
 
     def __getattr__(self, name):
-        name = name.replace('_', '.')
-        values = self.run("grep '{0}' /tmp/system.cfg".format(name))
+        config_name = name.replace('_', '.')
+        values = self.run('grep "{0}" /tmp/system.cfg'.format(config_name))
+
         if len(values) == 1:
-            name_length = len(name) + 1
-            value = values[0].rstrip()[name_length:]
-            if value == "enabled":
+            name_length = len(name) + 1 # Plus 1 to ignore '=' in config
+            value = values[0][name_length:]
+
+            if value == 'enabled':
                 return True
-            elif value == "disabled":
+            elif value == 'disabled':
                 return False
             else:
-                return values[0].rstrip()[name_length:]
+                return values[0][name_length:]
         else:
-            logging.warning("Attribute '%s' not found!", name)
-            return super(Device, self).__getattribute__(name)
+            raise AttributeError
 
     def __setattr__(self, name, value):
         try:
-            name = name.replace("_", ".")
+            self.getattr(self, name)
+        except AttributeError:
+            raise
+        else:
+            config_name = name.replace('_', '.')
 
             if value is True:
-                value = "enabled"
+                value = 'enabled'
             elif value is False:
-                value = "disabled"
+                value = 'disabled'
 
-            cmd = "sed -i '/{0}/ c {0}={1}' /tmp/system.cfg".format(name, value)
-            return self.run(cmd)
-        except RuntimeError:
-            super(Device, self).__setattr__(name, value)
+            cmd = 'sed -i "/{0}/ c {0}={1}" /tmp/system.cfg'.format(config_name, value)
+            self.run(cmd)
+
+    # def __delattr__(self, name):
+    #     if not isinstance(name, list):
+    #         name = name.replace('_', '.')
+    #         cmd = 'sed -i /{0}/d /tmp/system.cfg'.format(name)
+    #         self.run(cmd)
+    #     else:
+    #         raise AttributeError
 
     def connect(self):
         try:
-            self.session.connect(self.host,
-                                 username=self.username,
+            self.session.connect(self.host, username=self.username,
                                  password=self.password)
         except AuthenticationException:
-            logging.error("Incorrect username/password by host %s",
-                           self.host)
+            logging.error('Incorrect username/password by host %s', self.host)
         except (BadHostKeyException, SSHException) as e:
-            logging.error("Connection refused: %s. By host %s",
-                          e, self.host)
+            logging.error('Connection refused: %s. By host %s', e, self.host)
 
     def run(self, command):
         stdin, stdout, stderr = self.session.exec_command(command)
-        return stdout.readlines()
+        output = map(lambda x: x.rstrip(), stdout.readlines())
+        return list(output)
 
     def apply(self):
-        return self.run("cfgmtd -w")
+        return self.run('cfgmtd -w')
 
     # Don't like this...needs to be using SFTPClient()
-    def upload(self, local_file, to="/tmp/upload/"):
-        with open(local_file, "r") as f:
-            return self.run("printf '{0}' > {1}{2}".format("".join(f.read()),
+    def upload(self, local_file, to='/tmp/upload/'):
+        with open(local_file, 'r') as f:
+            return self.run('printf "{0}" > {1}{2}'.format(''.join(f.read()),
                 to, os.path.basename(local_file)))
 
     # Real upload to be implemented later
-    # def upload(self, local_file, to="/tmp/uploads"):
+    # def upload(self, local_file, to='/tmp/uploads'):
     #     file_session = self.session.open_sftp()
     #     file_session.put(local_file, '{0}/{1}'.format(to, local_file))
     #     file_session.close()
@@ -88,14 +104,4 @@ class Device(object):
     # Daemons
     def start_snmp_daemon(self):
         return self.run(
-            "tinysnmpd --daemon /etc/snmp.conf /lib/tinysnmp --logfile /tmp/snmp.log")
-
-
-class Radio(Device):
-    def __init__(self, ip, username, password):
-        super().__init__(ip, username, password)
-
-
-class Router(Device):
-    def __init__(self, ip, username, password):
-        super().__init__(ip, username, password)
+            'tinysnmpd --daemon /etc/snmp.conf /lib/tinysnmp --logfile /tmp/snmp.log')
