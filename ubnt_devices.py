4e3a311d617848c06f16b346362e2a846130cabc
nathanshimp <johnnshimp@gmail.com>
2015-04-27 01:15:26 -0400

    Redesigned for organic growth and a better API

--- ubnt_devices.py
+++ ubnt_devices.py
@@ -1,34 +1,31 @@
-import os
-import logging
+import textwrap
+from collections import namedtuple
 from paramiko import SSHClient, AutoAddPolicy
-from paramiko.ssh_exception import SSHException
-from paramiko.ssh_exception import AuthenticationException
-from paramiko.ssh_exception import BadHostKeyException
 
 
 class Device(object):
     def __init__(self, host, username, password):
-        self.__dict__["host"] = host
-        self.__dict__["username"] = username
-        self.__dict__["password"] = password
-        self.__dict__["session"] = SSHClient()
-        self.__dict__["transport"] = self.session.get_transport()
+        self.host = host
+        self.username = username
+        self.password = password
+        self.session = SSHClient()
         self.session.set_missing_host_key_policy(AutoAddPolicy())
         self.connect()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self):
-        self.disconnect()
 
     def __repr__(self):
         return '{0}{1}'.format(self.__class__.__name__, self.host)
 
-    def __getattr__(self, name):
-        config_name = name.replace('_', '.')
-        config_name = name.replace('0', '_')
-        values = self.run('grep "{0}" /tmp/system.cfg'.format(config_name))
+    def connect(self):
+        self.session.connect(self.host, username=self.username,
+                password=self.password)
+
+    def run(self, command):
+        stdin, stdout, stderr = self.session.exec_command(command)
+        output = [line.rstrip() for line in stdout.readlines()]
+        return list(output)
+
+    def read_conf(self, name, config="/tmp/system.cfg"):
+        values = self.run('grep "{0}=" {1}'.format(name, config))
 
         if len(values) == 1:
             name_length = len(name) + 1  # Plus 1 to ignore '=' in config
@@ -43,74 +40,125 @@ class Device(object):
         else:
             raise AttributeError
 
-    def __setattr__(self, name, value):
-        try:
-            getattr(self, name)
-        except AttributeError:
-            raise
-        else:
-            config_name = name.replace('_', '.')
-
-            if value is True:
-                value = 'enabled'
-            elif value is False:
-                value = 'disabled'
-
-            cmd = 'sed -i "/{0}/ c {0}={1}" /tmp/system.cfg'.format(config_name, value)
-            self.run(cmd)
-
-    def __delattr__(self, name):
-        try:
-            getattr(self, name)
-        except AttributeError:
-            raise
-        else:
-            name = name.replace('_', '.')
-            cmd = 'sed -i /{0}/d /tmp/system.cfg'.format(name)
-            self.run(cmd)
-
-    def connect(self):
-        try:
-            self.session.connect(self.host, username=self.username,
-                                 password=self.password)
-        except AuthenticationException:
-            logging.error('Incorrect username/password by host %s', self.host)
-        except (BadHostKeyException, SSHException) as e:
-            logging.error('Connection refused: %s. By host %s', e, self.host)
-
-    def run(self, command):
-        stdin, stdout, stderr = self.session.exec_command(command)
-        output = map(lambda x: x.rstrip(), stdout.readlines())
-        return list(output)
-
     def apply(self):
-        return self.run('cfgmtd -w')
+        return self.run('cfgmtd -w -p /etc/')
 
-    # Don't like this...needs to be using SFTPClient()
-    def upload(self, local_file, to='/tmp/upload/'):
-        with open(local_file, 'r') as f:
-            return self.run('printf "{0}" > {1}{2}'.format(''.join(f.read()),
-                to, os.path.basename(local_file)))
-
-    # Real upload to be implemented later
-    # def upload(self, local_file, to='/tmp/uploads'):
-    #     file_session = self.session.open_sftp()
-    #     file_session.put(local_file, '{0}/{1}'.format(to, local_file))
-    #     file_session.close()
+    def restart(self):
+        return self.run('/usr/etc/rc.d/rc.softrestart save')
 
     def disconnect(self):
         self.session.close()
 
-    def is_alive(self):
-        return self.transport.is_alive()
+    def version(self):
+        return self.run('cat /etc/version')[0]
 
-    def freq(self):
+    def hostname(self):
+        return self.read_conf('resolv.host.1.name')
+
+    def frequency(self):
+        if self.mode() == 'managed':
+            return self.live_frequency()
         try:
-            return self.radio_1_freq
+            freq = self.read_conf('radio.1.freq')
+            if freq != '0':
+                return freq
+            else:
+                return 'auto/{}'.format(self.live_frequency())
         except AttributeError:
-            return (self.radio_1_tx0freq, self.radio_1_rx0freq)  # Change to namedtuple
+            Frequency = namedtuple('Frequency', ['rx', 'tx'])
+            return Frequency(self.read_conf('radio.1.tx_freq'),
+                                self.read_conf('radio.1.rx_freq'))
 
-    # Daemons
-    def start_snmp_daemon(self):
-        return self.run(
-            'tinysnmpd --daemon /etc/snmp.conf /lib/tinysnmp --logfile /tmp/snmp.log')
+    def channel_width(self):
+        try:
+            clksel = self.read_conf('radio.1.clksel')
+            chanbw = self.read_conf('radio.1.chanbw')
+            if clksel == '4':
+                return '5'
+            elif clksel == '2':
+                if chanbw == '8':
+                    return chanbw
+                else:
+                    return '10'
+            elif clksel == '1':
+                if chanbw == '30':
+                    return chanbw
+                else:
+                    return '20'
+        except AttributeError:
+            try:
+                chanbw = self.read_conf('radio.1.chanbw')
+                return chanbw
+            except AttributeError:
+                rxchanbw = self.read_conf('radio.1.rxchanbw')
+
+                if rxchanbw == '256':
+                    rxchanbw = '40'
+                elif rxchanbw == '128':
+                    rxchanbw = '30'
+                elif rxchanbw == '64':
+                    rxchanbw = '20'
+                else:
+                    rxchanbw = '10'
+
+                txchanbw = self.read_conf('radio.1.txchanbw')
+
+                if txchanbw == '256':
+                    txchanbw = '40'
+                elif txchanbw == '128':
+                    txchanbw = '30'
+                elif txchanbw == '64':
+                    txchanbw = '20'
+                else:
+                    txchanbw = '10'
+
+                Bandwidth = namedtuple('Bandwidth', ['rx', 'tx'])
+                return Bandwidth(rxchanbw, txchanbw)
+
+    def channel_shift(self):
+        try:
+            if self.read_conf('radio.1.chanshift') == '0':
+                return 'disabled'
+            else:
+                return 'enabled'
+        except AttributeError:
+            return None
+
+    def distance(self):
+        model = self.model()
+        if model == 'airFiber 24G' or model == 'airFiber 5' or model == 'airFiber 5X':
+            return None
+        return self.read_conf('radio.1.ackdistance')
+
+    def distance_miles(self):
+        distance = self.distance()
+        if distance is not None:
+            return round(int(distance) * 0.00062137, 1)
+        else:
+            return None
+
+    def mac_address(self):
+        mac = self.read_conf('board.hwaddr', '/etc/board.info')
+        return ':'.join(textwrap.wrap(mac, 2))
+
+    def model(self):
+        return self.read_conf('board.name', '/etc/board.info')
+
+    def mode(self):
+        return self.read_conf('radio.1.mode')
+
+    def wds(self):
+        return self.read_conf('wireless.1.wds.status')
+
+    def live(self):
+        return self.run('iwconfig 2> /dev/null')[0]
+
+    def live_frequency(self):
+        cmd = 'iwconfig 2> /dev/null | grep Mode | cut -d ":" -f3 | cut -d " " -f1'
+        freq = self.run(cmd)[0].replace('.','')
+        if len(freq) == 3:
+            return freq + '0'
+        elif len(freq) == 2:
+            return freq + '00'
+        else:
+            return freq
